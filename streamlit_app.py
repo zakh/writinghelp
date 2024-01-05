a807aa7fe474e3823b5faafb230b5f56daccc1bd
zakh <zak.hubbard@gmail.com>
2024-01-05 12:48:03 -0800

    Fix grammar button behavior

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ grammar = st.button('Check Grammar')
 
 if grammar:
     st.write(st.session_state.text)
-    st.session_state.text = "back to normal"
+    st.session_state.text = 'back to normal'
     st.rerun()
     
     #matches = grammar_checker(st.session_state.text)
