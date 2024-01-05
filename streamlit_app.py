7f8b6c80e3caaf15d7a7d29eed03e734fb130954
zakh <zak.hubbard@gmail.com>
2024-01-04 16:24:44 -0800

    Refactor grammar_checker function to include a prefix in the output

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import textstat as ts
 
 # Function to call LanguageTool API for grammar checking
 def grammar_checker(text):
-    st.write(text)
+    st.write("the text", text)
     api_url = "https://api.languagetool.org/v2/check"
     payload = {
         'text': text,
